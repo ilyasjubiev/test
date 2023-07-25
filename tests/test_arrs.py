52eefe7c64a9341888b999354aa1bfb2dd3c6d7c
ilyas <jubiev2017@gmail.com>
2023-07-25 21:21:38 +0300

    Merge branch 'develop' of github.com:kirillskyeng/pytest_proj into develop

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,9 +6,13 @@ from utils import arrs
 def test_get():
     with pytest.raises(IndexError):
         assert arrs.get([1, 2, 3], 1, "test") == 2
+        assert arrs.get([1], -1, "test") == "test"
         assert arrs.get([], 0, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1], -5, 1) == [1]
